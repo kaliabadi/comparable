c6d1ec76eac81ef2ac88126e5079a7268f988b2c
kaliabadi <kouros_aliabadi@hotmail.com>
2018-05-29 13:18:18 +0100

    chore: experimentation

--- comparison.js
+++ comparison.js
@@ -1,13 +1,28 @@
 const fs = require('fs');
 const resemble = require('resemblejs');
+const looksSame = require('looks-same');
 
 const testFolder = './baseline/';
 
 const compare = (image1, image2) => {
-    resemble(image1).compareTo(image2).onComplete(function(data){
-        console.log(data.misMatchPercentage);
+    const fileName = image1.replace('./baseline/', '');
+    
+    looksSame.createDiff({
+        reference: image1,
+        current: image2,
+        diff: `./comparisons/${fileName}`,
+        highlightColor: '#ff00ff', //color to highlight the differences
+        strict: false,//strict comparsion
+        tolerance: 2.5
+    }, function(error) {
     });
 }
+
+// const compare = (image1, image2) => {
+//     resemble(image1).compareTo(image2).onComplete(function(data) {
+//         console.log(`${image1}: Mismatch percentage: ${data.misMatchPercentage}%`);
+//     });
+// }
 
 fs.readdir(testFolder, (err, files) => {
     files.forEach(file => {
--- snap.js
+++ snap.js
@@ -1,4 +1,4 @@
-const { Builder, By, until } = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver');
 
 const fs = require('fs');
 const scenarios = require('./scenarios.json').scenarios;
@@ -9,24 +9,56 @@ const writeScreenShot = (data, name) => {
     fs.writeFileSync(screenshotPath + name, data, 'base64');
 };
 
-const takeSnap= async (url, name) => {
-    // LOCAL
-    const driver = await new Builder().forBrowser('chrome').build()
-    
-    //REMOTE
-    // const driver = new webdriver.Builder().
-    //     usingServer('http://selenium-grid.tnl-dev.ntch.co.uk/wd.hub')
-    //     withCapabilities(webdriver.Capabilities.chrome()).
-    //     build();
+const removeSelectors = async(driver, selectors) => {
+    selectors.forEach(async(selector) => {
+        try {
+            let adElement = await driver.findElement(webdriver.By.id(elementId));
+            await driver.wait(webdriver.until.elementIsVisible(adElement), 5000);
+            await driver.executeAsyncScript(`var element = document.getElementById("${elementId}"); element.parentNode.removeChild(element)`);
+        } catch(error) {
+            console.error(error)
+        }
+    })
+}
 
-    await driver.get(url);
-    await driver.executeAsyncScript('document.getElementsByClassName("AD").remove()');
+const loadBrowser = async (driver, url) => {
+    try {
+        driver.manage().window().setRect({width: 1040, height: 2480});
+        await driver.get(url);
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+const takeSnap = async (driver, name) => {
     const snapshot = await driver.takeScreenshot()
     
-    await writeScreenShot(snapshot, `${name}.png`)
+    try {
+        await writeScreenShot(snapshot, `${name}.png`)
+        console.log(`Writing screenshot ${name}`)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const cleanUp = async(driver) => {
     await driver.quit()
 }
 
-scenarios.forEach(scenario => {
-    takeSnap(scenario.url, scenario.label)
-})
+const runSnapShots = async() => {
+    scenarios.forEach(async(scenario) => {
+        console.log(scenario.label)
+        const driver = await new webdriver.Builder().forBrowser('chrome').build()    
+        // const driver = new webdriver.Builder().
+        //     usingServer('http://selenium-grid.tnl-dev.ntch.co.uk:4444/wd/hub').
+        //     withCapabilities(webdriver.Capabilities.chrome()).
+        //     build();
+    
+        await loadBrowser(driver, scenario.url)
+        await removeSelectors(driver, scenarios.removeSelectors)
+        await takeSnap(driver, scenario.label)
+        await cleanUp(driver);
+    })
+}
+
+runSnapShots();
